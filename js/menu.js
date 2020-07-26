5f22accb68afcc04bd05ecea5745bd6b6a1d30f2
PFreire98 <freiregabriel98@gmail.com>
2020-07-25 21:15:02 -0300

    Folder organization update

---
+++ js/menu.js
@@ -0,0 +1,14 @@
+/* Animação Menu */
+function myFunctionNav() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+      x.className += " responsive";
+  } else {
+      x.className = "topnav";
+  }
+}
+
+/* Animação Sandwich */
+function myFunctionBars(x) {
+  x.classList.toggle("change");
+}
